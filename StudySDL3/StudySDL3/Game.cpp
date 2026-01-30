de92dfdcb76b1e6e2cf01c90c136a02788b52c80
shay0454 <shay0454@naver.com>
2026-01-30 21:50:48 +0900

    feature: 게임 루프 구조 잡기
    (Initialize, RunLoop, Shutdown)

---
+++ StudySDL3/StudySDL3/Game.cpp
@@ -0,0 +1,57 @@
+﻿#include"Game.h"
+#include<SDL3/SDL.h>
+
+Game::Game() {}
+
+// 초기화
+bool Game::Initialize() {
+	//SDL 초기화
+	bool sdlResult = SDL_Init(SDL_INIT_VIDEO);
+	if (sdlResult == false) {
+		SDL_Log("Unable to initialize SDL : %s", SDL_GetError());
+		return false;
+	}
+
+	//Window 생성
+	mWindow = SDL_CreateWindow("Game Programming", 1024, 768, 0);
+	if (!mWindow) {
+		SDL_Log("Failed to create window : %s", SDL_GetError());
+	}
+
+	return true;
+}
+
+// 게임 루프
+void Game::RunLoop() {
+	while (mIsRunning) {
+		ProcessInput();			// 입력을 처리합니다.
+		UpdateGame();			// 게임을 업데이트합니다.
+		GenerateOutput();		// 업데이트 결과를 출력합니다.
+	}
+}
+
+// 게임 종료
+void Game::Shutdown() {
+	SDL_DestroyWindow(mWindow);	// 메모리 및 운영체제 자원 반환
+	SDL_Quit();					// 서브시스템 종료 및 지원 강제 해제
+}
+
+void Game::ProcessInput()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_EVENT_QUIT:
+			mIsRunning = false;
+			break;
+		}
+	}
+}
+
+void Game::UpdateGame()
+{
+}
+
+void Game::GenerateOutput()
+{
+}
---
+++ StudySDL3/StudySDL3/Game.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#ifndef GAME_H	// game.h 헤더 파일이 포함 안 되었을 경우에만 포합시킵니다.
+#define GAME_H	// game.h 정의
+class Game {
+public:
+	Game();				// 생성자
+	bool Initialize();	// 초기화
+	void RunLoop();		// 게임 루프
+	void Shutdown();	// 게임 종료
+private:
+	void ProcessInput();	// 입력 처리
+	void UpdateGame();		// 게임 업데이트
+	void GenerateOutput();	// 게임 로직 처리
+
+	class SDL_Window* mWindow;
+	bool mIsRunning;
+
+};
+#endif // !GAME_H
